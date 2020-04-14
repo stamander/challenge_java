82ed60047bca8bd3dc1ce2cd24d6c82d4b90f2b9
stamander <iida.atsushi.fh@gmail.com>
2020-04-14 12:50:34 +0900

    Update challenge.java

--- challenge.java
+++ challenge.java
@@ -2,5 +2,20 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Hello Java");
     System.out.println(3*3);
+    System.out.println(12/3);
+    
+    // 3に6を掛けた値を出力してください
+    System.out.println(3*6);
+    
+    // 8を3で割った時の余りを出力してください
+    System.out.println(8%3);
+    System.out.println("こんにちは"+"世界");
+    
+    // "38"と"19"を連結して出力してください
+    System.out.println("38"+"19");
+    
+    
+    // 38と19を足して出力してください
+    System.out.println(38+19);
   }
 }
