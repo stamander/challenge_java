e073fd48588cc5029236c72be2427e6673ce3760
stamander <iida.atsushi.fh@gmail.com>
2020-04-14 12:55:33 +0900

    Update challenge.java

--- challenge.java
+++ challenge.java
@@ -1,21 +1,23 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello Java");
-    System.out.println(3*3);
-    System.out.println(12/3);
+    int number;
     
-    // 3に6を掛けた値を出力してください
-    System.out.println(3*6);
-    
-    // 8を3で割った時の余りを出力してください
-    System.out.println(8%3);
-    System.out.println("こんにちは"+"世界");
-    
-    // "38"と"19"を連結して出力してください
-    System.out.println("38"+"19");
+    // 変数numberに3を代入してください
+    number = 3;
     
     
-    // 38と19を足して出力してください
-    System.out.println(38+19);
+    // 変数numberを出力してください
+    System.out.println(number);
+    
+
+    // String型の変数nameを定義してください
+    String name;
+    
+    // 変数nameに"Wanko"を代入してください
+    name = "Wanko";
+    
+    // 変数nameを出力してください
+    System.out.println(name);
   }
 }
