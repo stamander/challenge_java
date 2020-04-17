55587415595920dfdafe0a7ff995211ea9d12d2f
stamander <iida.atsushi.fh@gmail.com>
2020-04-17 13:28:54 +0900

    Update challenge.java

--- challenge.java
+++ challenge.java
@@ -16,9 +16,9 @@ class Main {
     System.out.println(text + "Progateで");
 
     int number = 11;
-    String text = "Ruby";
+    String text1 = "Ruby";
     System.out.println(number);
-    System.out.println(text);
+    System.out.println(text1);
     
     // 変数numberを9で上書きしてください
     number = 9;
@@ -27,37 +27,13 @@ class Main {
     System.out.println(number);
     
     // 変数textを「Java」で上書きしてください
-    text = "Java";
+    text1 = "Java";
     
     // 変数textを出力してください
-     System.out.println(text);
+     System.out.println(text1);
     
     
   }
-}
-    
-  }
-}
 
-class Main {
-  public static void main(String[] args) {
-    int number = 11;
-    String text = "Ruby";
-    System.out.println(number);
-    System.out.println(text);
     
-    // 変数numberを9で上書きしてください
-    number = 9;
-    
-    // 変数numberを出力してください
-    System.out.println(number);
-    
-    // 変数textを「Java」で上書きしてください
-    text = "Java";
-    
-    // 変数textを出力してください
-     System.out.println(text);
-    
-    
-  }
 }
