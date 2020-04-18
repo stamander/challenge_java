ea2c453460031c001630659425e0a86452e14115
stamander <iida.atsushi.fh@gmail.com>
2020-04-18 15:19:34 +0900

    Update challenge.java

--- challenge.java
+++ challenge.java
@@ -1,19 +1,15 @@
 class Main {
   public static void main(String[] args) {
-    int length = 6;
-    int height = 8;
+    double number1 = 8.5;
     
-    // 変数rectangleAreaに、四角形の面積を代入してください
-    int rectangleArea = length * height;
+    // double型の変数number2を定義し、3.4を代入してください。
+    double number2 = 3.4;
     
-    // 変数rectangleAreaを出力してください
-    System.out.println(rectangleArea);
+    // number1にnumber2を足した値を出力してください
+    System.out.println(number1+number2);
     
-    // 変数triangleAreaに、三角形の面積を代入してください
-    int triangleArea = length * height /2;
-    
-    // 変数triangleAreaを出力してください
-    System.out.println(triangleArea);
+    // number1からnumber2を引いた値を出力してください
+    System.out.println(number1-number2);
   }
 
     
