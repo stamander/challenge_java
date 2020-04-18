2e060f46eba0cd60326e0a8798c8748919ff7984
stamander <iida.atsushi.fh@gmail.com>
2020-04-18 15:08:10 +0900

    Update challenge.java

--- challenge.java
+++ challenge.java
@@ -1,38 +1,18 @@
 class Main {
   public static void main(String[] args) {
-    int number1 = 3;
+    int number = 8;
     
-    // int型の変数number2を定義し、7を代入してください
-    int number2 = 7;
-    
-    // number1 * number2を出力してください
-    System.out.println(number1 * number2);
-    
-    
-    // 変数textに「プログラミングを勉強しよう」を代入してください
-    String text = "プログラミングを勉強しよう";
-    
-    // 「Progateで」と変数textを連結して出力してください
-    System.out.println(text + "Progateで");
-
-    int number = 11;
-    String text1 = "Ruby";
-    System.out.println(number);
-    System.out.println(text1);
-    
-    // 変数numberを9で上書きしてください
-    number = 9;
+    // 変数numberに7をかけて、変数numberを上書きしてください
+    number *=7;
     
     // 変数numberを出力してください
     System.out.println(number);
     
-    // 変数textを「Java」で上書きしてください
-    text1 = "Java";
+    // 変数numberの値に1を足して、変数numberを上書きしてください
+    number ++;
     
-    // 変数textを出力してください
-     System.out.println(text1);
-    
-    
+    // 変数numberを出力してください
+    System.out.println(number);
   }
 
     
