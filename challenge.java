cdc7de02f325579e4fac12770795f53718c0d745
stamander <iida.atsushi.fh@gmail.com>
2020-04-18 15:29:53 +0900

    Update challenge.java

--- challenge.java
+++ challenge.java
@@ -1,19 +1,11 @@
 class Main {
   public static void main(String[] args) {
-    int month = 12;
-    int date = 31;
+    int number1 = 7;
+    int number2 = 2;
+    System.out.println(number1 / number2);
     
-    // 「12月31日」となるように変数と文字列を連結して出力してください
-    System.out.println(month +"月"+ date +"日");
-    
-    // 7を2で割った値を出力してください
-    System.out.println(7/2);
-    
-    // 7.0を2.0で割った値を出力してください
-    System.out.println(7.0/2.0);
-    
-    // 7を2.0で割った値を出力してください
-    System.out.println(7/2.0);
+    // number1をdouble型にキャストし、number2で割った値を出力してください
+    System.out.println((double)number1/number2);
   }
 
     
