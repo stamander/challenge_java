e40916d9c516c5a4fbc6886f8ac8063de2adaed4
stamander <iida.atsushi.fh@gmail.com>
2020-04-19 11:35:03 +0900

    Update challenge.java

--- challenge.java
+++ challenge.java
@@ -3,16 +3,19 @@ class Main {
   public static void main(String[] args) {
     int number = 12;
     
-    // numberが20より小さいとき、どちらでもないときの条件分岐を追加してください
-    if (number < 10) {
-      System.out.println("10より小さい");
-    }
-    else if(number < 20){
-      System.out.println("10以上、20より小さい");
+    int number = 12;
     
-    }
-    else{
-      System.out.println("20以上");
+    // switch文を用いて、numberを3で割った余りで条件分岐をしてください
+    switch (number%3){
+      case 0:
+        System.out.println("3で割り切れます");
+        break;
+       case 1:
+        System.out.println("3で割ると1余ります");
+        break;
+       case 2:
+        System.out.println("3で割ると2余ります");
+        break;
     }
     
     
