d2a9efefb512076a72375d3e732db0a3d65424e6
stamander <iida.atsushi.fh@gmail.com>
2020-04-19 12:36:16 +0900

    Create hash.java

---
+++ hash.java
@@ -0,0 +1,17 @@
+
+class Main {
+  public static void main(String[] args) {
+    // 変数languagesを定義し、配列を代入してください
+    String[] langages ={"Ruby", "PHP", "Python"};
+    
+    // インデックス番号が「1」の要素を出力してください
+    System.out.println(langages[1]);
+    
+    // インデックス番号が「1」の要素を「Java」で上書きしてください
+    langages[1] ="Java";
+    
+    // インデックス番号が「1」の要素を出力してください
+    System.out.println(langages[1]);
+    
+  }
+}
