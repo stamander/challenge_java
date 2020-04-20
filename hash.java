53862edab60f6f7ac4570c0afe0c9ebfe130acd5
stamander <iida.atsushi.fh@gmail.com>
2020-04-20 13:15:06 +0900

    Update hash.java

--- hash.java
+++ hash.java
@@ -1,12 +1,23 @@
 class Main {
   public static void main(String[] args) {
-    String[] names = {"にんじゃわんこ", "ひつじ仙人", "ベイビーわんこ"};
+    // 変数numbersに、与えられた数字の配列を代入してください
+    int[] numbers = {1, 4, 6, 9, 13, 16};
     
-    // for文を用いて、「私の名前は◯◯です」と出力してください
-    for (int i=0;i<names.length;i++){
-      System.out.println("私の名前は"+names[i]+"です");
+    int oddSum = 0;
+    int evenSum = 0;
+    
+    // for文を用いて、配列numbersの偶数の和と奇数の和を求めてください
+    for (int number:numbers){
+      if (number %2==0){
+        oddSum +=number;
+        
+      }
+      else{
+         evenSum+=number;
+        
+      }
     }
-    
-    
+    System.out.println("偶数の和は" + evenSum + "です");
+    System.out.println("奇数の和は" + oddSum + "です");
   }
 }
