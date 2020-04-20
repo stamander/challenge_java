60ae3036199e59ddcbd8c2f59465a0b4797c983b
stamander <iida.atsushi.fh@gmail.com>
2020-04-20 11:06:37 +0900

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -28,3 +28,18 @@ const level = 12;
 if (level>10){
   console.log('レベルが10より大きいです');
 }
+
+
+
+const age = 24;
+
+// 「age >= 20」を出力してください
+console.log(age>= 20);
+
+// 「age < 20」を出力してください
+console.log(age<20);
+
+// ageの値が20以上の場合に、「私は20歳以上です」と出力してください
+if (age>20){
+  console.log("私は20際以上です");
+}
