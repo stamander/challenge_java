d7111021ecb57ac0464108efb8f0f65eca06b4e9
stamander <iida.atsushi.fh@gmail.com>
2020-04-20 11:15:32 +0900

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -60,3 +60,14 @@ if (password != "ninjawanko"){
   console.log("パスワードが間違っています");
 }
 
+
+
+const age = 17;
+
+// 条件式が成り立たない場合に「私は20歳未満です」と出力してください
+if (age >= 20) {
+  console.log("私は20歳以上です");
+} else{
+  console.log("私は20歳未満です");
+}
+
