fc81b5ca1d6f610aff6faff5e817843864a4c560
stamander <iida.atsushi.fh@gmail.com>
2020-04-20 10:25:00 +0900

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -1,5 +1,8 @@
-// 変数nameを定義し、「にんじゃわんこ」を代入してください
-let name = "にんじゃわんこ";
+// 変数lengthを定義してください
+let length = 5;
 
-// 変数nameの値を出力してください
-console.log(name);
+// 変数lengthの値を出力してください
+console.log(length);
+
+// 変数lengthを用いて、円の面積を出力してください
+console.log(length*length*3);
