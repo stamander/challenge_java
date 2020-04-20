1dc92cc11078d2a9acaf2d12b41b01aa976d92e2
stamander <iida.atsushi.fh@gmail.com>
2020-04-20 10:42:38 +0900

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -10,3 +10,13 @@ console.log(number+=3);
 
 
 console.log(number/2);
+
+
+const language= "フランス語";
+
+// 定数languageの値を出力してください
+console.log(language);
+
+// 定数languageを用いて、「〇〇を話せます」と出力してください
+
+console.log(language + "を話せます");
