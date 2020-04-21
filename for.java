e6f8f8f2a3a4ccbb5a50b3f6237ba0cc7e735e86
stamander <iida.atsushi.fh@gmail.com>
2020-04-21 09:55:36 +0900

    Update for.java

--- for.java
+++ for.java
@@ -2,36 +2,10 @@
 
 class Main {
   public static void main(String[] args) {
-    System.out.println("=== while文 ===");
-    int i = 1;
-    while (i < 10) {
-      // iが5の倍数のとき、繰り返し処理を終了してください
-      if (i%5 ==0){
-        break;
-      }
-      
-      System.out.println(i);
-      i++;
+    // for文を用いて、繰り返し処理をつくってください
+    for (int i= 1;i<=10;i++){
+      System.out.println(i+"回目のループです");
     }
     
-    System.out.println("=== for文 ===");
-    for (int j = 1; j < 10; j++) {
-      // jが3の倍数のとき、処理をスキップしてください
-      if (j%3==0){
-        continue;
-      }
-      
-      System.out.println(j);
   }
-
-  String[]names = {"にんじゃわんこ","ひつじ仙人","ベイビーわんこ"};
-    
-  // インデックス番号が0の要素を出力してください
-  System.out.println(names[0]);
-  
-  // インデックス番号が2の要素を出力してください
-  System.out.println(names[2]);
 }
-
-}
-
