92e1fc71c3ac65dc12462805a13678c09d185429
stamander <iida.atsushi.fh@gmail.com>
2020-04-21 10:19:32 +0900

    Update hash.java

--- hash.java
+++ hash.java
@@ -1,16 +1,11 @@
 class Main {
   public static void main(String[] args) {
-    // 変数languagesを定義し、配列を代入してください
-    String[] languages={"Ruby", "PHP", "Python"};
+    String[] names = {"にんじゃわんこ", "ひつじ仙人", "ベイビーわんこ"};
     
-    // インデックス番号が「1」の要素を出力してください
-    System.out.println(languages[1]);
-    
-    // インデックス番号が「1」の要素を「Java」で上書きしてください
-    languages[1]= "Java";
-    
-    // インデックス番号が「1」の要素を出力してください
-    System.out.println(languages[1]);
+    // for文を用いて、「私の名前は◯◯です」と出力してください
+    for(int i = 0; i<names.length;i++){
+      System.out.println("私の名前は"+names[i]+"です");
+    }
     
   }
 }
