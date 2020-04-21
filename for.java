454d60aa96160a57e4b903c4186dee9317075d8b
stamander <iida.atsushi.fh@gmail.com>
2020-04-21 10:52:19 +0900

    Update for.java

--- for.java
+++ for.java
@@ -28,5 +28,23 @@ class Main {
     for (String name:names){
       System.out.println("私の名前は"+name+"です");
     }
+    int[] numbers = {1, 4, 6, 9, 13, 16};
+    
+    int oddSum = 0;
+    int evenSum = 0;
+    
+    // for文を用いて、配列numbersの偶数の和と奇数の和を求めてください
+    for(int number:numbers){
+      if (number%2==0){
+       evenSum+=number;
+      }
+      else{
+         oddSum+=number;
+      }
+    }
+
+    System.out.println("奇数の和は" + oddSum + "です");
+    System.out.println("偶数の和は" + evenSum + "です");
+  
   }
 }
