37cfbefc1464107f55c20dfc442aa15e71adb686
stamander <iida.atsushi.fh@gmail.com>
2020-05-07 16:45:03 +0900

    Update method.java

--- method.java
+++ method.java
@@ -1,15 +1,5 @@
 class Main {
-  public static void main(String[] args) {
-    // 引数に「Kate Jones」を渡してください
-    printPrice("ピザ",3000);
-    printPrice("コーラ",150);
-    
-  }
 
-  // 引数を受け取るようにしてください
-  public static void printPrice(String name,int yen) {
-    // 「私の名前は◯◯です」と出力されるように書き換えてください
-    System.out.println(name+"は"+yen+"円です" );
-    
-  }
+  public
+
 }
