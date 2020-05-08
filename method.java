604414f5e5dd318b510c5289d26c3aef4e08fe87
stamander <iida.atsushi.fh@gmail.com>
2020-05-08 09:50:57 +0900

    Update method.java

--- method.java
+++ method.java
@@ -1,19 +1,16 @@
 class Main {
 
   public static void main(String[] args){
-    hello();
-    hello();
+ 
     printData();
   }
 
-  public static void hello(){
-    System.out.println("Hello world");
-  }
+ 
 
   public static void printData(){
     System.out.println("my name is atsushi");
   }
 
-  
+
 
 }
