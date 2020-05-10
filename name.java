c7db7f3747de65f5b476f89f740bd85dfa3c5243
stamander <iida.atsushi.fh@gmail.com>
2020-05-10 09:55:24 +0900

    Update name.java

--- name.java
+++ name.java
@@ -1,18 +1,11 @@
 import java.util.Scanner;
 
-class Main{
-  public static void main(String[] args){
+class Main {
+  public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    System.out.println("年齢");
-    int age  =scanner.nextInt();
-    System.out.println("体重");
-    double weight =scanner.nextDouble();
-    System.out.println("私の年齢は" + age +"です" );
-    System.out.println("私の体重は" + weight + "です");
-    System.out.println("名前")
-    String name = scanner.next();
-    System.out.println("あなたの名前は" + name +"です");
+    System.out.println("名前：");
+    String firstName = scanner.next();
+    System.out.println("名前は"+firstName+"です");
+    
   }
-  
 }
-
