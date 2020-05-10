58a5f46dd0196d6d5f5a9929c55d15c670f3a05b
stamander <iida.atsushi.fh@gmail.com>
2020-05-10 09:37:43 +0900

    Update name.java

--- name.java
+++ name.java
@@ -8,8 +8,8 @@ class Main{
     System.out.println("体重");
     double weight =scanner.nextDouble();
     System.out.println("私の年齢は" + age +"です" );
-
-
+    System.out.println("私の体重は" + weight + "です");
+    System.out.println("名前")
     String name = scanner.next();
     System.out.println("あなたの名前は" + name +"です");
   }
