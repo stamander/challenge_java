e67ef70e2c8a41da13d5a5ed3c78fd95dd697701
stamander <iida.atsushi.fh@gmail.com>
2020-05-10 09:11:54 +0900

    Update name.java

--- name.java
+++ name.java
@@ -4,7 +4,8 @@ class Main{
   public static void main(String[] args){
     Scanner scanner = new Scanner(System.in);
     System.out.println("あなたの名前：");
-    String name = 
+    String name = Scanner.next();
+    System.out.println("あなたの名前は"　＋なめ)
   }
   
 }
