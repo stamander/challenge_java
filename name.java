9540c86ae62cb871e88be4a99d1cd43ede50cf9a
stamander <iida.atsushi.fh@gmail.com>
2020-05-10 09:25:52 +0900

    Update name.java

--- name.java
+++ name.java
@@ -3,7 +3,13 @@ import java.util.Scanner;
 class Main{
   public static void main(String[] args){
     Scanner scanner = new Scanner(System.in);
-    System.out.println("あなたの名前：");
+    System.out.println("年齢");
+    int age  =scanner.nextInt();
+    System.out.println("体重");
+    double weight =scanner.nextDouble();
+    System.out.println("私の年齢は" + age +"です" );
+
+
     String name = scanner.next();
     System.out.println("あなたの名前は" + name +"です");
   }
