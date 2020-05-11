0f62fedce98adb8d7e2471d85a95b6e655adacf0
stamander <iida.atsushi.fh@gmail.com>
2020-05-12 08:35:42 +0900

    Update object.java

--- object.java
+++ object.java
@@ -2,10 +2,13 @@ public class object {
   public static void main(final String[] args) {
 
     Person person1 = new Person();
+    person1.name = "ats";
     Person person2 = new Person();
     Person person3 = new Person();
 
     person1.hello();
+
+    System.out.println(person1.name);
 
 
 
