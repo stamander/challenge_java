b8f1dd0aab85cf078e167b2b5726004eac273d5d
stamander <iida.atsushi.fh@gmail.com>
2020-05-12 08:16:58 +0900

    クラス呼び出し

--- object.java
+++ object.java
@@ -1,7 +1,7 @@
 public class object {
-  public static void main(String[] args){
+  public static void main(final String[] args) {
 
-    Person person = new Person();
+    final Person person = new Person();
     person.hello();
 
 
