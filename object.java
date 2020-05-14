9970f037dde3e267db2946f7e8e897259f2970fa
stamander <iida.atsushi.fh@gmail.com>
2020-05-14 19:14:00 +0900

    Update object.java

--- object.java
+++ object.java
@@ -1,28 +1,9 @@
 class Main {
   public static void main(final String[] args) {
 
-    Person person1 = new Person("Kate", "JOhn");
+    Person person1 = new Person();
 
-    System.out.println(person1.firstName);
-
-    Person person2 = new Petson("John","Chirstpher","Smith",)
-
-    public int count =0;
-    public String firstName;
-    public String lastName;
-   
-
-  System.out.println(person.getMiddleName());
-
-  private String middleName;
-
-  public void setMiddleName(String middleName){
-    this.middleName = middleName;
-  }
-
-  
-
-
+    Person person2 
 
 
 
