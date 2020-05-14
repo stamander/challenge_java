f44e919db500e2d25faf59f49952f95d097b547e
stamander <iida.atsushi.fh@gmail.com>
2020-05-14 19:20:49 +0900

    Update main.java

--- main.java
+++ main.java
@@ -1,10 +1,12 @@
-import java.lang.Math;
-
-
-
-class Main{
-  public static void main(String[] args){
-    double a = (6.2);
-    System.out.println(a + "を四捨五入すると"+Math.round(a));
+class Main {
+  public static void main(String[] args) {
+    Person person1 = new Person();
+    // person1に対して、helloメソッドを呼び出してください
+    person1.hello();
+    
+    Person person2 = new Person();
+    // person2に対して、helloメソッドを呼び出してください
+    person2.hello();
+    
   }
 }
