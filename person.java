110045207bda67aa2e705005feeae188325694b7
stamander <iida.atsushi.fh@gmail.com>
2020-05-15 11:26:58 +0900

    Update person.java

--- person.java
+++ person.java
@@ -5,12 +5,21 @@ class Person {
   public String name;
   public String firstName;
   public String lastName;
+  public String 
   
   public void hello(){
     System.out.println("こんにちは"+this.name);
   }
 
   Person(String name, String firstName, String lastName){
+    System.out.println("インスタンスが生成されました");
+    this.name = name;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    Person.count ++;
+  }
+
+  Person(String name, String firstName, String middleName, String lastName){
     System.out.println("インスタンスが生成されました");
     this.name = name;
     this.firstName = firstName;
