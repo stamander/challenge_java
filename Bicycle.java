d318115219f374021d7bb662b6c980d5c6bd0d32
stamander <iida.atsushi.fh@gmail.com>
2020-05-16 20:28:21 +0900

    name

--- Bicycle.java
+++ Bicycle.java
@@ -1,7 +1,14 @@
 class Bicycle{
   private String name;
-  
-  public String name(){
-    System.out.println("hello");
+
+  Bicycle(String name){
+    this.name = name;
+
   }
+
+  public String getName{
+    
+  }
+
+
 }
