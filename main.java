0ee9278f8f18154a8a33c36f6f11568ff8664a6a
stamander <iida.atsushi.fh@gmail.com>
2020-05-16 20:08:28 +0900

    Update main.java

--- main.java
+++ main.java
@@ -1,19 +1,5 @@
 class Main {
   public static void main(String[] args) {
-    Person person1 = new Person("ats");
-    // person1に対して、helloメソッドを呼び出してください
-
-    System.out.println(Person.count);
     
-    System.out.println(person1.name);
-    person1.hello();
-    
-    Person person2 = new Person("yuki");
-    // person2に対して、helloメソッドを呼び出してください
-    person2.hello();
-    
-    person2.setMiddleName("ats");
-
-    System.out.println(person2.getMiddleName());
   }
 }
