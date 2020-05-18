61051bc1a2ef1c354b38ccbf2fc5064e9cf31da4
stamander <iida.atsushi.fh@gmail.com>
2020-05-18 19:42:51 +0900

    name

--- Bicycle.java
+++ Bicycle.java
@@ -1,13 +1,21 @@
 public class Bicycle{
 
   private String name;
+  private String color;
 
-  Bicycle(String name){
+  Bicycle(String name ,String color){
     this.name = name;
+    this.color = color;
   }
 
-  public String getName(){
+  public String Name(){
     return this.name;
+  }
+
+  public void printData(){
+    System.out.println("【自転車の情報】");
+    System.out.println("名前：" + );
+    System.out.printLn("色：" + this.color);
   }
 }
 
--- main.java
+++ main.java
@@ -1,8 +1,9 @@
 
 public class Main {
   public static void main(String[] args) {
-    Bicycle bicycle = new Bicycle("ビアンキ");
-    System.out.println("【自転車の情報】");
-    System.out.println("名前：" + bicycle.getName());
+    Bicycle bicycle = new Bicycle("ビアンキ", "緑");
+
+
+    bicycle.printData();
   }
 }
