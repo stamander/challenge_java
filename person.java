8176243771ddc771d4d8be9062a7437e764c1613
stamander <iida.atsushi.fh@gmail.com>
2020-05-18 10:52:09 +0900

    コンパイル

--- person.java
+++ person.java
@@ -1,6 +1,6 @@
 public class Person {
   public static void hello() {
     // "Hello World"を、"Hello Java"に書き換えてください
-    System.out.println("Hello Java");
+    System.out.println("Hello world");
   }
 }
