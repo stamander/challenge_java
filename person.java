0ae79adb10d4ad923d771f18340d2f234370234b
stamander <iida.atsushi.fh@gmail.com>
2020-05-18 12:50:55 +0900

    継承

--- person.java
+++ person.java
@@ -1,6 +1,25 @@
 public class Person {
-  public static void hello() {
-    // "Hello World"を、"Hello Java"に書き換えてください
-    System.out.println("Hello world");
+  public static void printData(String firstName,String lastName, int age, double height, double weight){
+    System.out.println("名前は" + fullName(firstName ,lastName) + "です");
+    System.out.println("年齢は"+age+"歳です");
+
+    if(age>=20){
+      System.out.println("成年者です");
+    }else{
+      System.out.println("未成年です");
+    }
+
+    System.out.println("身長は"+height+"mです");
+    System.out.println("体重は"+weight+"kgです");
+    Double bmi = bmi(height,weight);
+    System.out.println("BMIは"+Math.round(bmi)+"です");
+
+  }
+
+  public static String fullName(String firstName,String lastName){
+    return firstName + lastName;
+  }
+  public static double bmi(double height,double weight){
+    return weight/height/height;
   }
 }
