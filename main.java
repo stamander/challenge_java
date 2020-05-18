06634ebafaffe289bd146dbade81cdfe71a3fa75
stamander <iida.atsushi.fh@gmail.com>
2020-05-18 10:16:36 +0900

    Update main.java

--- main.java
+++ main.java
@@ -1,10 +1,9 @@
 class Main {
   public static void main(String[] args) {
-    
-    Bicycle bicycle = new Bicycle("ビアンキ");
-    System.out.println("【自転車の情報】");
-    
-    System.out.println("名前：" + bicycle.getName());
+    Car car = new Car();
+    car.setName("フェラーリ");
+    car.setColor("赤");
+
     
   }
 }
