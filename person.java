cca1dd5778855f8b383226711b9028dd3aa736fd
stamander <iida.atsushi.fh@gmail.com>
2020-05-18 13:10:37 +0900

    java

--- person.java
+++ person.java
@@ -1,3 +1,4 @@
+
 public class Person {
   public static void printData(String firstName,String lastName, int age, double height, double weight){
     System.out.println("名前は" + fullName(firstName ,lastName) + "です");
@@ -14,6 +15,12 @@ public class Person {
     Double bmi = bmi(height,weight);
     System.out.println("BMIは"+Math.round(bmi)+"です");
 
+    if (isHealthy(bmi)){
+      System.out.println("健康です");
+    }else{
+      System.out.println("健康でない");
+    }
+
   }
 
   public static String fullName(String firstName,String lastName){
@@ -21,5 +28,9 @@ public class Person {
   }
   public static double bmi(double height,double weight){
     return weight/height/height;
+  }
+
+  public static boolean isHealthy(double bmi){
+    return bmi >= 18.5 && bmi<25.0;
   }
 }
