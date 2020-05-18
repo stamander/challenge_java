fba3e9981c140b1f9308c62710813e38f7e08a0c
stamander <iida.atsushi.fh@gmail.com>
2020-05-18 17:14:16 +0900

    javac

--- main.java
+++ main.java
@@ -1,9 +1,7 @@
 class Main {
-  public static void main(String[] args){
-
-
-
+  public static void main(String[] args) {
+    Bicycle bicycle = new Bicycle("ビアンキ");
+    System.out.println("【自転車の情報】");
+    System.out.println("名前：" + bicycle.getName());
   }
-
-
 }
