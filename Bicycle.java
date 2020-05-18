a9a912d684a21ba4844e4a12f97f7abe09ca9895
stamander <iida.atsushi.fh@gmail.com>
2020-05-18 19:43:14 +0900

    Update Bicycle.java

--- Bicycle.java
+++ Bicycle.java
@@ -15,7 +15,7 @@ public class Bicycle{
   public void printData(){
     System.out.println("【自転車の情報】");
     System.out.println("名前：" + this.name);
-    System.out.printLn("色：" + this.color);
+    System.out.println("色：" + this.color);
   }
 }
 
