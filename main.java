8fb24223d39c526c3fe4423c06d523584cb512c3
stamander <iida.atsushi.fh@gmail.com>
2020-05-18 17:37:23 +0900

    deleate

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
-class Main {
+public class Main {
   public static void main(String[] args) {
     Bicycle bicycle = new Bicycle("ビアンキ");
     System.out.println("【自転車の情報】");
-    System.out.println("名前：" + bicycle.getName());
+    System.out.println("名前：" + bicycle.name());
   }
 }
