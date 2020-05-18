6b4ec5d3fde24d291f6f75e121b9953ea6134694
stamander <iida.atsushi.fh@gmail.com>
2020-05-18 11:53:16 +0900

    Update main.java

--- main.java
+++ main.java
@@ -1,8 +1,20 @@
-
+import java.util.Scanner;
 class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args){
 
-    System.out.println("名前はiidaです");
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("名前:");
+
+    String firstName = scanner.next();
+
+    System.out.print("名字");
+    
+
+    String lastName = scanner.next();
+
+    System.out.println("名前は" + firstName + " " + lastName + "です");
+
+
 
   }
 }
