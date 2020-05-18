5a40c848f2ba2d2a558f40f9337b58ae78763b2c
stamander <iida.atsushi.fh@gmail.com>
2020-05-18 11:37:16 +0900

    Update main.java

--- main.java
+++ main.java
@@ -1,20 +1,8 @@
 
 class Main {
   public static void main(String[] args) {
-    Car car = new Car();
-    // setNameメソッドを用いて、carの名前を「フェラーリ」にしてください
-    car.setName("フェラーリ");
-    
-    // setColorメソッドを用いて、carの色を「赤」にしてください
-    car.setColor("赤");
-    
 
-    
-    System.out.println("【車の情報】");
-    car.printData();
-
-    System.out.println("=================");
-    System.out.println("【自転車の情報】");
+    System.out.println("名前はiidaです");
 
   }
 }
