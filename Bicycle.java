7fa9926229f640b4332f6b18f046b83823a99fa3
stamander <iida.atsushi.fh@gmail.com>
2020-05-18 17:07:42 +0900

    name

--- Bicycle.java
+++ Bicycle.java
@@ -1,19 +1,11 @@
 class Bicycle{
+
   private String name;
-  private String color;
-  
+
   Bicycle(String name){
     this.name = name;
   }
-  public String getName(){
-    return this.name;
-  }
 
-  public String getCorlor(){
-    return this.color;
-  }
+  
+}
 
-  public void printData(){
-    System.out.println("走る距離を入力してください");
-  }
-}d
--- main.java
+++ main.java
@@ -1,5 +1,3 @@
-
-
 class Main {
   public static void main(String[] args){
 
