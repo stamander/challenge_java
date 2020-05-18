771f215add1252729d962be3aa9278e222db43e9
stamander <iida.atsushi.fh@gmail.com>
2020-05-18 13:47:00 +0900

    Update main.java

--- main.java
+++ main.java
@@ -4,44 +4,6 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args){
 
-    Scanner scanner = new Scanner(System.in);
-
-    System.out.println("何人の情報を入力しますか？");
-    int person = scanner.nextInt();
-
-    int maxAge = 0;
-    int totalAge = 0;
-
-    for(int i =0; i<person;i++){
-
-    System.out.println(i + 1 +"人目");
-
-    System.out.print("名前:");
-    String firstName = scanner.next();
-
-    System.out.print("名字");
-    String lastName = scanner.next();
-
-    System.out.print("年齢:");
-    int age = scanner.nextInt();
-
-    System.out.print("身長:");
-    double height = scanner.nextDouble();
-
-    System.out.print("体重:");
-    double weight = scanner.nextDouble();
-
-    Person.printData(firstName,lastName,age,height,weight);
-    if(age>maxAge){
-      maxAge = age;
-    }
-    totalAge +=age;
-  }
-
-  System.out.println("最高年齢は"+maxAge+"です");
-  System.out.println("平均年齢は"+totalAge/person+"です");
-
-
 
 
   }
