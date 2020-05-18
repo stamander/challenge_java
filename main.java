26c2bd02d07d3af5890b6f3cb9167b0b50bbaec7
stamander <iida.atsushi.fh@gmail.com>
2020-05-18 12:02:16 +0900

    Update main.java

--- main.java
+++ main.java
@@ -3,16 +3,26 @@ class Main {
   public static void main(String[] args){
 
     Scanner scanner = new Scanner(System.in);
-    System.out.print("名前:");
 
+    System.out.print("名前:");
     String firstName = scanner.next();
 
     System.out.print("名字");
-    
-
     String lastName = scanner.next();
 
+    System.out.print("年齢:");
+    int age = scanner.nextInt();
+
+    
+
     System.out.println("名前は" + firstName + " " + lastName + "です");
+    System.out.println("年齢は"+age+"歳です");
+
+    if(age>=20){
+      System.out.println("成年者です");
+    }else{
+      System.out.println("未成年です");
+    }
 
 
 
