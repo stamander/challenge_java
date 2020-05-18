19041c283f44a94fbd07711e45149f835f4e893d
stamander <iida.atsushi.fh@gmail.com>
2020-05-18 12:31:56 +0900

    Update main.java

--- main.java
+++ main.java
@@ -19,13 +19,14 @@ class Main {
     System.out.print("体重:");
     double weight = scanner.nextDouble();
 
+    printData(firstName,lastName,age,height,weight);
 
 
-    
+  }
 
-    System.out.println("名前は" + firstName + " " + lastName + "です");
+  public static void printData(String firstName,String lastName, int age, double height, double weight){
+    System.out.println("名前は" + fullName(firstName ,lastName) + "です");
     System.out.println("年齢は"+age+"歳です");
-
 
     if(age>=20){
       System.out.println("成年者です");
@@ -36,5 +37,9 @@ class Main {
     System.out.println("身長は"+height+"mです");
     System.out.println("体重は"+weight+"kgです");
 
+  }
+
+  public static String fullName(String firstName,String lastName){
+    return firstName + lastName;
   }
 }
