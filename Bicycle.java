c5f1e6630650f19bc449e3307a8fe0cdbde0e968
stamander <iida.atsushi.fh@gmail.com>
2020-05-18 19:48:34 +0900

    conpaile
    java cのときは　javac ファイル名.java
    java 実行の時は　java ファイル名

--- Bicycle.java
+++ Bicycle.java
@@ -11,7 +11,7 @@ public class Bicycle{
 
 
   public void printData(){
-    System.out.println("【自転車の情報】");
+    
     System.out.println("名前：" + this.name);
     System.out.println("色：" + this.color);
   }
--- main.java
+++ main.java
@@ -3,6 +3,8 @@ public class Main {
   public static void main(String[] args) {
     Bicycle bicycle = new Bicycle("ビアンキ", "緑");
 
+    System.out.println("【自転車の情報】");
+
 
     bicycle.printData();
   }
