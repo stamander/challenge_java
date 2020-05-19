e851d676bafbbe9ad70ebc0abde11995812e8d69
stamander <iida.atsushi.fh@gmail.com>
2020-05-19 12:34:49 +0900

    java

--- main.java
+++ main.java
@@ -2,14 +2,11 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-    Car car = new Car();
-    car.setName("フェラーリ");
-    car.setColor("赤");
+    Car car = new Car("フェラーリ","赤");
 
 
-    Bicycle bicycle = new Bicycle();
-    bicycle.setName("ビアンキ");
-    bicycle.setColor("緑");
+   
+
 
 
     System.out.println("【車の情報】");
@@ -25,10 +22,6 @@ class Main {
     car.charge(fuel);
     
 
-    System.out.println("=================");
-    System.out.println("【自転車の情報】");
-    bicycle.printData();
 
-   
   }
 }
