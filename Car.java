6968db2801d2de70a13c1a860e2e49445882e79c
stamander <iida.atsushi.fh@gmail.com>
2020-05-19 10:16:20 +0900

    Update Car.java

--- Car.java
+++ Car.java
@@ -3,5 +3,10 @@ public class Car {
   private String name;
   private String color;
   private int distance =0;
+
+  Car(String name ,String color){
+    this.name = name;
+    this.color = color;
+  }
   
 }
