18c4e3cc4d366a1af139151449dba1d2bd5714f6
stamander <iida.atsushi.fh@gmail.com>
2020-05-19 12:21:41 +0900

    Update Car.java

--- Car.java
+++ Car.java
@@ -6,7 +6,12 @@ public class Car extends Vehicle{
     return this.fuel;
   }
 
-  public void 
+  public void printData(){
+    System.out.println("名前："+this.getName());
+    System.out.println("色："+this.getColor());
+    System.out.println("走行距離："+this.getDistance());
+    System.out.println("ガソリン量："+this.fuel);
+  }
 
   public void charge(int fuel){
     if(fuel<0){
