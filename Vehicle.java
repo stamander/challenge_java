3ddaa116148004e03a1122b4ad0cfeb9e066ee83
stamander <iida.atsushi.fh@gmail.com>
2020-05-20 16:45:39 +0900

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -27,7 +27,9 @@ public void printData() {
   System.out.println("走行距離：" + this.distance + "km");
 }
 
-Vehicle(){
+Vehicle(String name,String color){
+  this.name = name;
+  this.color=color;
   
 }
 
