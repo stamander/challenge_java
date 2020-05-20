609e82bf9137bd8edd8d0ae776165cc2daa48410
stamander <iida.atsushi.fh@gmail.com>
2020-05-20 16:59:05 +0900

    Update Car.java

--- Car.java
+++ Car.java
@@ -18,7 +18,9 @@ public class Car extends Vehicle{
     if(fuel<distance){
       System.out.println("ガソリンが足りません");
     }else if(fuel>=distance){
+      System.out.println(distance + "km走ります");
       this.distance-=distance;
+      
 
     }
 
