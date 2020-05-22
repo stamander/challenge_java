7bdcd372b90bc796ab2af612b3a7b696fc682e11
stamander <iida.atsushi.fh@gmail.com>
2020-05-22 12:29:30 +0900

    java

--- first.java
+++ first.java
@@ -1,3 +1,11 @@
-public class first {
+class first {
+  public static void main(String[] args) {
+
+  String name = "Stamander";
+
+  System.out.println(name);
+
+
+  }
   
 }
---
+++ first.rb
@@ -0,0 +1,3 @@
+name = "Stamander"
+
+p name
