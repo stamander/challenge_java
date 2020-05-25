41f0881cdf3cb14e5b876c040a0d8a46f38232b6
stamander <iida.atsushi.fh@gmail.com>
2020-05-25 10:07:18 +0900

    Update first.java

--- first.java
+++ first.java
@@ -3,13 +3,13 @@
 class first {//クラス
   public static void main(String[] args) {//メソッド
 
-    String name = "Stamnder";  //変数の宣言
+    String name = "stamander";  //変数の宣言
+    System.out.println("私の名前は"+name+"です"); //name出力
+
+    int age = 5;
     System.out.println("私の名前は"+name+"です"); //name出力
     
-    System.out.println("私の名前はStamanderです"); 
-    System.out.println("私の名前はStamanderです"); 
-    System.out.println("私の名前はStamanderです"); 
-    System.out.println("私の名前はStamanderです"); 
+
 
 
 
