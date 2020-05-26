3b19ad014f272498b13d1dcbc0f98257fea85c91
stamander <iida.atsushi.fh@gmail.com>
2020-05-26 09:31:08 +0900

    Update first.java

--- first.java
+++ first.java
@@ -4,11 +4,18 @@ class first {//クラス
   public static void main(String[] args) {//メソッド
 
 
-    
+    System.out.println("私の名前はStamanderです"); 
+    System.out.println("私の名前はStamanderです"); 
+    System.out.println("私の名前はStamanderです"); 
+    System.out.println("私の名前はStamanderです"); 
 
-    for(int i = 0; i<4; i++){
-      System.out.println("私の名前はStamanderです"); 
-    }
+
+
+    // for(int i = 0; i<4; i++){
+    //   System.out.println("私の名前はStamanderです"); 
+    // }
+
+
 
 
 
@@ -16,3 +23,4 @@ class first {//クラス
   }
   
 }
+
