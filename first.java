2707ee5c21274d90584a4387a7f991f080091f1e
stamander <iida.atsushi.fh@gmail.com>
2020-05-27 12:41:00 +0900

    Update first.java

--- first.java
+++ first.java
@@ -3,12 +3,12 @@
 class first {//クラス
   public static void main(String[] args) {
 
-    int number = 2;
+    int number = 0;
 
     if(number>=1){
       System.out.println("numberは1よりも大きいです");
     }else{
-      
+
     }
 
 
