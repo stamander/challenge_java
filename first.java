0e5379584f6981d0cf5b7ef59ca4b6c5c67d0840
stamander <iida.atsushi.fh@gmail.com>
2020-05-27 12:50:42 +0900

    Update first.java

--- first.java
+++ first.java
@@ -3,14 +3,6 @@
 class first {//クラス
   public static void main(String[] args) {
 
-    int number = 3;
-
-    if(number%2==0){
-      System.out.println("numberは偶数です");
-    }else{
-      System.out.println("numberは奇数です");
-
-    }
 
     for(int i = 0; i<50;i++){
 
