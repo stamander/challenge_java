abfb5b87974dd837598b940d1d74fe45a7dc24f6
stamander <iida.atsushi.fh@gmail.com>
2020-05-27 12:50:33 +0900

    Update first.java

--- first.java
+++ first.java
@@ -12,7 +12,16 @@ class first {//クラス
 
     }
 
-    for(int i =)
+    for(int i = 0; i<50;i++){
+
+    if(i%2==0){
+      System.out.println("numberは偶数です");
+    }else{
+      System.out.println("numberは奇数です");
+
+    }
+
+    }
 
 
 
