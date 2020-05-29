4dbdf76e55ab3bb7c470139b7ede3bd40980a4e0
stamander <iida.atsushi.fh@gmail.com>
2020-05-29 19:36:24 +0900

    Update first.java

--- first.java
+++ first.java
@@ -4,17 +4,6 @@ class first {//クラス
   public static void main(String[] args) {
 
 
-    for(int i = 0; i<=50;i++){
-
-    if(i%2==0){
-      System.out.println(i+",この数字は偶数です");
-    }else{
-      System.out.println(i+",この数字は奇数です");
-
-    }
-
-    }
-
 
 
     hello();
@@ -33,7 +22,7 @@ class first {//クラス
   }
 
   public static void hello(){
-    System.out.println("hello.Stamander");
+    System.out.println("hello Stamander");
   }
   
 }
