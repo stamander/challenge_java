51a28989f3d1967fd24cebcb408a3c84ccd5302a
stamander <iida.atsushi.fh@gmail.com>
2020-05-30 19:07:51 +0900

    Update first.java

--- first.java
+++ first.java
@@ -4,10 +4,16 @@ class first {
   public static void main(String[] args) {
 
     hello();
+
+    
   }
 
   public static void hello(){
     System.out.println("hello Stamander");
+  }
+
+  public static ats(int a){
+    return a*2;
   }
   
 }
