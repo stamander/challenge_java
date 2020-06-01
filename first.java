ce9c094b539fa32b3cea68aba2c889d55df68541
stamander <iida.atsushi.fh@gmail.com>
2020-06-01 12:59:40 +0900

    Update first.java

--- first.java
+++ first.java
@@ -1,13 +1,14 @@
 class first {
   public static void main(String[] args) {
 
-    hello();
+   
 
     ats("atsuhsi",5);
     ats("yukimi",23);
 
     for(int i = 0;i<5;i++){
-      System.out.println(i)
+      System.out.println(i);
+      hello();
     }
   }
 
